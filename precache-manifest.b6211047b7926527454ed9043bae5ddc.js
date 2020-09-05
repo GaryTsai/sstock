b48230ff6acc99931b979e4e78d2c20541acd23e
gary_tsai <gary_tsai@perfectcorp.com>
2020-09-06 02:32:00 +0800

    Updates

---
+++ precache-manifest.b6211047b7926527454ed9043bae5ddc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cba098b66dfae73434e17e96c9776fc",
+    "url": "/sstock/index.html"
+  },
+  {
+    "revision": "6fb858ac59c4d5d225ef",
+    "url": "/sstock/static/css/2.2f73783b.chunk.css"
+  },
+  {
+    "revision": "aaaa46c62220e9ea0237",
+    "url": "/sstock/static/css/main.08d7b77d.chunk.css"
+  },
+  {
+    "revision": "6fb858ac59c4d5d225ef",
+    "url": "/sstock/static/js/2.d4eec9d6.chunk.js"
+  },
+  {
+    "revision": "037118a42577f921d1cfedec32b33f3b",
+    "url": "/sstock/static/js/2.d4eec9d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaaa46c62220e9ea0237",
+    "url": "/sstock/static/js/main.f696473d.chunk.js"
+  },
+  {
+    "revision": "1390a31bbfbcb01414ee",
+    "url": "/sstock/static/js/runtime-main.d490fed9.js"
+  }
+]);
