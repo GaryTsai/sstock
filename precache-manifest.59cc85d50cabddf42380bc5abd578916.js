1a481bb1dfc16ce5b4b99c77fe4a7f12ea10246c
gary_tsai <gary_tsai@perfectcorp.com>
2020-09-15 21:28:10 +0800

    Updates

--- precache-manifest.59cc85d50cabddf42380bc5abd578916.js
+++ precache-manifest.59cc85d50cabddf42380bc5abd578916.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "53d2bcb935431dc6cca01aa9641f286a",
+    "revision": "d168cff0f4313f321418ced3622ef8c6",
     "url": "/sstock/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sstock/static/css/2.2f73783b.chunk.css"
   },
   {
-    "revision": "8fe3545e70781be56266",
+    "revision": "e2cd079a70e504c2f05e",
     "url": "/sstock/static/css/main.08d7b77d.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sstock/static/js/2.95898000.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8fe3545e70781be56266",
-    "url": "/sstock/static/js/main.a97a0621.chunk.js"
+    "revision": "e2cd079a70e504c2f05e",
+    "url": "/sstock/static/js/main.9046b89f.chunk.js"
   },
   {
     "revision": "1390a31bbfbcb01414ee",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sstock/precache-manifest.80f90817e7d19ecd3bdb5238765079ef.js"
+  "/sstock/precache-manifest.59cc85d50cabddf42380bc5abd578916.js"
 );
 
 self.addEventListener('message', (event) => {
