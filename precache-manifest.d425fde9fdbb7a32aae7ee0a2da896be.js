0920a7c04b966b819afab42efde3dc5a008f4587
GaryTsai <garybird606@gmail.com>
2024-07-15 04:09:34 +0000

    Deploying to gh-pages from  @ 735ec0523c07821a8a696ad9cfd4887aa989e6d6 🚀

---
+++ precache-manifest.d425fde9fdbb7a32aae7ee0a2da896be.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab47c47ad2b995d3c6b86582f55dedac",
+    "url": "/sstock/index.html"
+  },
+  {
+    "revision": "c02bd92f415f670c1cce",
+    "url": "/sstock/static/css/2.db713b68.chunk.css"
+  },
+  {
+    "revision": "c4d60801c94040ee9b60",
+    "url": "/sstock/static/css/main.72174a69.chunk.css"
+  },
+  {
+    "revision": "c02bd92f415f670c1cce",
+    "url": "/sstock/static/js/2.4b1cdac4.chunk.js"
+  },
+  {
+    "revision": "f04e496f9d21b0a60bf7e4079b7b8847",
+    "url": "/sstock/static/js/2.4b1cdac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4d60801c94040ee9b60",
+    "url": "/sstock/static/js/main.f6b7b9db.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/sstock/static/js/main.f6b7b9db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d387f1458036c653ea62",
+    "url": "/sstock/static/js/runtime-main.ce3411f0.js"
+  },
+  {
+    "revision": "aee8afdb2af73d2d83a827d24ada3b44",
+    "url": "/sstock/static/media/contentLoading.aee8afdb.png"
+  },
+  {
+    "revision": "a57e7c9adadfb2fbc47a738d3a2cb430",
+    "url": "/sstock/static/media/loading.a57e7c9a.gif"
+  },
+  {
+    "revision": "e27c8735da98ec6ccdcf12e258b26475",
+    "url": "/sstock/static/media/logo.e27c8735.png"
+  }
+]);
