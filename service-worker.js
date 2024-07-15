c5a7d29cbcebd25e1d3f51b31ae95ff596afba9c
GaryTsai <garybird606@gmail.com>
2024-07-15 04:17:32 +0000

    Deploying to gh-pages from  @ 3bc488c0d305b8dec75c44cdc6d55194bb3d5a41 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sstock/precache-manifest.d425fde9fdbb7a32aae7ee0a2da896be.js"
+  "/sstock/precache-manifest.cc0201a896e27c2da065052674c06fea.js"
 );
 
 self.addEventListener('message', (event) => {
